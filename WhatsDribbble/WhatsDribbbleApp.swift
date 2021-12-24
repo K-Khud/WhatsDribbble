c3a4f32d06702fa61c3a13da32c4292c7b354fae
Ekaterina Khudzhamkulova <katerina.koreneva@gmail.com>
2021-12-24 08:46:21 +0200

    Initial Commit

---
+++ WhatsDribbble/WhatsDribbbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsDribbbleApp.swift
+//  WhatsDribbble
+//
+//  Created by Ekaterina Khudzhamkulova on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WhatsDribbbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
